f8bd2700794ca9c2a4ebb45e3f0e036360cbf9f8
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-06-07 16:15:52 -0300

    Style first slide

--- talk.jl
+++ talk.jl
@@ -24,7 +24,7 @@ begin
 	using MeshViz
 	using PlutoUI
 	
-	# default vizualization options
+	# default vizualization settings
 	import WGLMakie as WGL
 	theme = WGL.Theme(
 		resolution = (650,500)
@@ -32,22 +32,30 @@ begin
 	WGL.set_theme!(theme)
 end
 
+# ╔═╡ e02bb839-dbe3-4e05-ad48-e39020d605d1
+html"""
+<img src="https://juliacon.org/assets/shared/img/logo_20.svg"> <font size=20>2021</font>
+"""
+
 # ╔═╡ b85daaf1-d07b-44d0-9cef-cc19087d792d
 md"""
 # Geostatistical Learning
 
-[Júlio Hoffimann, Ph.D.](https://juliohm.github.io)
+Júlio Hoffimann, Ph.D. ([julio.hoffimann@impa.br](mailto:julio.hoffimann@impa.br))
+
+*Postdoctoral fellow in Industrial Mathematics*
+
+Instituto de Matemática Pura e Aplicada
 """
 
-# ╔═╡ 48346d09-8487-487a-8c1e-abc7ef26e377
-BRA = GeoTables.gadm("BRA", children=true);
-
-# ╔═╡ f2dc93af-24c4-4c21-8dc6-fad124923998
-viz(BRA.geometry)
+# ╔═╡ 26ff713f-9ab8-460d-a748-bca8217d4ee5
+html"""
+<img src="https://icm2018.impa.br/images/logo-impa.png", width=200>
+"""
 
 # ╔═╡ Cell order:
 # ╟─d088c772-c7b4-11eb-1796-7365ee1abe49
 # ╟─995980f5-aa29-46a1-834e-9458c71f8914
+# ╟─e02bb839-dbe3-4e05-ad48-e39020d605d1
 # ╟─b85daaf1-d07b-44d0-9cef-cc19087d792d
-# ╠═48346d09-8487-487a-8c1e-abc7ef26e377
-# ╠═f2dc93af-24c4-4c21-8dc6-fad124923998
+# ╟─26ff713f-9ab8-460d-a748-bca8217d4ee5
